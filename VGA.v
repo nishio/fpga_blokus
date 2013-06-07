b515621632779846dd8d3cd57a9cd615635ab0da
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2013-06-07 18:36:51 +0900

    remove trailing space

--- VGA.v
+++ VGA.v
@@ -6,7 +6,7 @@ module VGA(clk, rgb_out, hsync, vsync, write_addr, wdata, write_en, reset_vram);
 	// VRAM
 	input [19:0]write_addr;
 	input wdata, write_en, reset_vram;
-	
+
 	reg vga_clk, i_hs, i_vs, i_hdisp, i_vdisp;
 	reg rgb_reg;
 	reg [9:0]hcount, vcount;
@@ -14,7 +14,7 @@ module VGA(clk, rgb_out, hsync, vsync, write_addr, wdata, write_en, reset_vram);
 	wire [9:0]dx, dy;
 	wire q_sig;
 	wire [2:0]color;
-	
+
 	wire [19:0]read_addr;
 	RAM2pot	RAM2pot_inst (
 		.aclr ( reset_vram ),
@@ -25,18 +25,18 @@ module VGA(clk, rgb_out, hsync, vsync, write_addr, wdata, write_en, reset_vram);
 		.wren ( write_en ),
 		.q ( q_sig )
 	);
-	
+
 	always @(posedge clk) begin
 		vga_clk = ~vga_clk;
 	end
-	
+
 	always @(posedge vga_clk) begin
 		if(hcount == 799)
 			hcount = 0;
 		else
 			hcount = hcount + 10'd1;
 	end
-	
+
 	always @(posedge vga_clk) begin
 		if(hcount == 0)
 			i_hs = 0;
@@ -44,34 +44,34 @@ module VGA(clk, rgb_out, hsync, vsync, write_addr, wdata, write_en, reset_vram);
 			i_hs = 1;
 	end
 
-	always @(posedge vga_clk) begin	
+	always @(posedge vga_clk) begin
 		if(hcount == 144)
 			i_hdisp = 1;
 		else if(hcount == 784)
-			i_hdisp = 0;			
+			i_hdisp = 0;
 	end
-	
+
 	always @(posedge i_hs) begin
 		if(vcount == 520)
 			vcount = 0;
 		else
 			vcount = vcount + 1'd1;
 	end
-	
+
 	always @(posedge i_hs) begin
 		if(vcount == 10)
 			i_vs = 0;
 		else if(vcount == 2)
 			i_vs = 1;
 	end
-	
+
 	always @(posedge i_hs) begin
 		if(vcount == 31)
 			i_vdisp = 1;
 		else if(vcount == 511)
 			i_vdisp = 0;
 	end
-	
+
 	assign x = hcount - 10'd144;
 	assign y = vcount - 10'd35;
 	//	assign read_addr = x + y * 20'd640;
