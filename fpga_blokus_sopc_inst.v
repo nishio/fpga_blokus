3d3f9f8f55c50bc1648be4abceef78eefa9cef13
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2013-06-09 18:38:27 +0900

    add files generated by SOPC builder

---
+++ fpga_blokus_sopc_inst.v
@@ -0,0 +1,9 @@
+  //Example instantiation for system 'fpga_blokus_sopc'
+  fpga_blokus_sopc fpga_blokus_sopc_inst
+    (
+      .clk_0                   (clk_0),
+      .in_port_to_the_pio_1    (in_port_to_the_pio_1),
+      .out_port_from_the_pio_0 (out_port_from_the_pio_0),
+      .reset_n                 (reset_n)
+    );
+
