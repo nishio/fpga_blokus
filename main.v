f7a742b20f4739481ec466f3a4b53f205eacc757
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2013-06-16 17:54:03 +0900

    make RGB 4bits

--- main.v
+++ main.v
@@ -1,7 +1,13 @@
-module main(dip, led);
+module main(switch, dip, clk, led, hsync, vsync, rgb);
+	input [2:0]switch;
 	input [9:0]dip;
+	input clk;
+	
 	output [9:0]led;
+	output hsync, vsync;
+	output [11:0] rgb;
 	
 	assign led = dip;
 	
+	//VGA vga_inst(clk);
 endmodule
