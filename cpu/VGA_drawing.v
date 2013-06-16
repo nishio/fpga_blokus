3ea8fa0df7fda9f17457fab671ed736bd1ae84c5
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2013-06-16 22:26:47 +0900

    debugging

--- cpu/VGA_drawing.v
+++ cpu/VGA_drawing.v
@@ -1,13 +1,14 @@
 
 	assign board_x = (x - 95) / 32;
 	assign board_y = (y - 15) / 32;
-	assign  board_vram_addr = x + y * 8'd14;
+	//assign  board_vram_addr = x + y * 8'd14;
 
 	assign color =
 	x >= 95 && y >= 15 && x < 545 && y < 465 ? (
 		(x - 95) % 32 < 2 || (y - 15) % 32 < 2 ? 12'hccc :
 		board_vram_out & 6'b100000 ? 12'hf70 :
 		board_vram_out & 6'b000100 ? 12'h70f :
+		board_vram_out ? 12'h77f :
 		12'hddd
 	) :
 	x < 95 ? (x + y):
