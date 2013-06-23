41bb8f0b35d098a5f5e8578a5a1d9172c8da1c48
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2013-06-23 23:34:43 +0900

    add 4 gpio

--- main.v
+++ main.v
@@ -1,14 +1,14 @@
 module main(switch, dip, gpio, clk, led, hsync, vsync, rgb);
 	input [2:0]switch;
 	input [9:0]dip;
-	input [3:0]gpio;
+	input [7:0]gpio;
 	input clk;
 
 	output [9:0]led;
 	output hsync, vsync;
 	output [11:0] rgb;
 	
-	assign led = dip;
-	
+	//assign led = dip;
+	assign led[7:0] = gpio[7:0];
 	//VGA vga_inst(clk, hsync, vsync, rgb);
 endmodule
