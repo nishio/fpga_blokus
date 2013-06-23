71a6d2871cff2c6bedec6ec14300b14aac51ae7b
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2013-06-23 21:33:56 +0900

    add gpio pins

--- main.v
+++ main.v
@@ -1,13 +1,14 @@
-module main(switch, dip, clk, led, hsync, vsync, rgb);
+module main(switch, dip, gpio, clk, led, hsync, vsync, rgb);
 	input [2:0]switch;
 	input [9:0]dip;
+	input [3:0]gpio;
 	input clk;
-	
+
 	output [9:0]led;
 	output hsync, vsync;
 	output [11:0] rgb;
 	
 	assign led = dip;
 	
-	VGA vga_inst(clk, hsync, vsync, rgb);
+	//VGA vga_inst(clk, hsync, vsync, rgb);
 endmodule
